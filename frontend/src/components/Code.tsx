36c3babdb3208ae491eea5d6d7a00f4e406725f5
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-04-11 11:43:17 +0530

    ui improved

--- frontend/src/components/Code.tsx
+++ frontend/src/components/Code.tsx
@@ -1,8 +1,8 @@
 const Code = () => {
     return (
-        <div className="flex-1 bg-gray-900 overflow-y-auto overflow-x-auto">
-          <div className="h-full p-1">
-            <div className="h-full bg-gray-950 border border-transparent rounded-lg p-4">
+        <div className="w-7/12 bg-gray-900 overflow-y-auto overflow-x-auto">
+          <div className="h-full">
+            <div className="h-full bg-gray-950 border border-transparent p-4">
               <pre className="text-sm text-gray-300">
                 <code>{`<!DOCTYPE html>
 <html lang="en">
--- frontend/src/components/Toggle.tsx
+++ frontend/src/components/Toggle.tsx
@@ -3,7 +3,7 @@ import { EyeIcon, Code } from "lucide-react";
 
 const Toggle = ({ setToggle }: { setToggle: React.Dispatch<React.SetStateAction<boolean>> }) => {
     return (
-        <div className="flex bg-gray-950 h-full border-x border border-gray-700 rounded-lg justify-center">
+        <div className="flex bg-gray-950 h-full border-x border border-gray-700 rounded-lg justify-center cursor-pointer">
             <div onClick={() => setToggle(true)} className="flex items-center space-x-2 bg-gray-950 border border-transparent text-white rounded-lg py-2 px-4 hover:bg-white hover:text-black">
                 <span>Code</span><Code />
             </div>
